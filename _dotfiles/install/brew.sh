fc8efa16ec93fb85b37f5d70baafb0f3e4428b05
Alfredo <dev@alfredordz.com>
2020-03-05 16:34:57 -0800

    Fixed brew conditional

--- _dotfiles/install/brew.sh
+++ _dotfiles/install/brew.sh
@@ -1,12 +1,14 @@
-!/bin/bash
+#!/bin/bash
 
 # Check if Homebrew is installed, if not install it
-if test ! $(which brew); then
- /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | sh
-fi
+if [[ $(command -v brew) == "" ]]; then
+ 	echo "Installing Homebrew..."
+ 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" | sh
 
-# Update Homebrew recipes
-brew update
+else
+	echo "Updating Homebrew recipes..."
+	brew update
+fi
 
 # Install all dependencies with bundle (see Brewfile)
 brew tap homebrew/bundle
