4f036653ab306e32632f658406de315b2a934bc5
Alfredo <dev@alfredordz.com>
2020-03-05 22:41:54 -0800

    Fixed install scripts

--- _dotfiles/install/brew.sh
+++ _dotfiles/install/brew.sh
@@ -3,8 +3,9 @@
 # Check if Homebrew is installed, if not install it
 if [[ $(command -v brew) == "" ]]; then
  	echo "Installing Homebrew..."
- 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" | sh
+ 	/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+        echo "brew ready to use!"
 else
 	echo "Updating Homebrew recipes..."
 	brew update
@@ -12,6 +13,6 @@ fi
 
 # Install all dependencies with bundle (see Brewfile)
 brew tap homebrew/bundle
-brew bundle
+brew bundle install
 
-echo "Success! Basic Brew packages and Brew additional applications installed! ! !"
+echo "Success!!! Basic Brew packages and Brew additional applications installed."
--- _dotfiles/install/oh-my-zsh.sh
+++ _dotfiles/install/oh-my-zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Oh My Zsh
-curl -fdSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Make Zsh defautl shell
 sudo chsh -s $(which zsh)
